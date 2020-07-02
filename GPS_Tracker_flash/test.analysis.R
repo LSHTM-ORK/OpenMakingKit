304ac6f3c648815c89beb9ab61ed6bc494b4b666
Chrissy h Roberts <31275801+chrissyhroberts@users.noreply.github.com>
2020-07-02 10:49:12 +0100

    improve

--- GPS_Tracker_flash/test.analysis.R
+++ GPS_Tracker_flash/test.analysis.R
@@ -1,4 +1,5 @@
 library(measurements)
+library(leaflet)
 ########################################################################################
 #define substring right function
 substrRight<-function(x, n){substr(x, nchar(x)-n+1, nchar(x))}
@@ -6,21 +7,25 @@ substrRight<-function(x, n){substr(x, nchar(x)-n+1, nchar(x))}
 # define nmea gps conversion function
 convert.nmea.gps<-function(loc,nsew)
 {
-  loc<-gsub(pattern = substrRight(x = as.character(loc),n = 7),replacement = paste(" ",substrRight(x = as.character(loc),n = 7),sep=""),x = as.character(loc),ignore.case = T)
-  loc.conv<-measurements::conv_unit(loc, from = 'deg_dec_min', to = 'dec_deg')
-  if(nsew=="S"){loc.conv<-paste("-",loc.conv,sep="")}
-  if(nsew=="W"){loc.conv<-paste("-",loc.conv,sep="")}
+  pointplace<-unlist(gregexpr(pattern ='\\.',loc))
+  n.characters<-nchar(loc)
 
+  degrees<-substr(loc,start = 1,stop = pointplace-3)
+  if(degrees==""){degrees<-0}
+  mins<-substr(loc,pointplace-2,n.characters)
+  mins<-as.numeric(mins)/60
+  loc<-as.numeric(degrees)+mins
 
-  return(loc.conv)
+  if(nsew=="W" | nsew == "S"){loc<--loc}
+  return(loc)
 }
 ########################################################################################
 
-gps<-read.csv("test.data.csv",header=F,na.strings = "")
+gps<-read.csv("test.data2.csv",header=F,na.strings = "")
 
-cp<-gps[grep(gps$V1,pattern = "CP"),]
+#cp<-gps[grep(gps$V1,pattern = "CP"),]
 
-gps<-gps[-grep(gps$V1,pattern = "CP"),]
+#gps<-gps[-grep(gps$V1,pattern = "CP"),]
 gps<-as.data.frame(gps)
 names(gps)<-c("time","lat","n_s","lon","w_e","fix_quality","num_satellites","horz_dilution_position","altitude_m","height_geoid","time_since_last_DGPS_update","dgps_station_id_number","empty","checksum")
 
@@ -31,37 +36,25 @@ gps<-gps[!is.na(gps$time),]
 
 gps$time[which(nchar(gps$time)==5)]<-paste("0",gps$time[which(nchar(gps$time)==5)],sep="")
 
-#gps$time<-strptime(gps$time,format="%H%M%S")
-
+gps$time<-strptime(gps$time,format="%H%M%S")
 gps<-gps[-which(is.na(gps$lat)),]
+
 # convert from degrees and decimal minutes to degrees
 
-gpsconv<-as.data.frame("lat")
-gpsconv$lon<-NA
 for(i in 1:nrow(gps))
 {
-convert.nmea.gps(lat = gps$lat[i],ns = gps$n_s[i],lon = gps$lon[i],we = gps$w_e[i])
-}
+  gps$lat[i]<-convert.nmea.gps(loc = gps$lat[i],nsew = gps$n_s)
+  gps$lon[i]<-convert.nmea.gps(loc = gps$lon[i],nsew = gps$w_e)
+  }
+
+gps$lat<-as.numeric(gps$lat)
+gps$lon<-as.numeric(gps$lon)
 
 
-#GGA          Global Positioning System Fix Data
-#123519       Fix taken at 12:35:19 UTC
-#4807.038,N   Latitude 48 deg 07.038' N
-#     01131.000,E  Longitude 11 deg 31.000' E
-#1            Fix quality: 0 = invalid
-#1 = GPS fix (SPS)
-#2 = DGPS fix
-#3 = PPS fix
-#4 = Real Time Kinematic
-#5 = Float RTK
-#6 = estimated (dead reckoning) (2.3 feature)
-#7 = Manual input mode
-#8 = Simulation mode
-#08           Number of satellites being tracked
-#0.9          Horizontal dilution of position
-#545.4,M      Altitude, Meters, above mean sea level
-#46.9,M       Height of geoid (mean sea level) above WGS84
-#ellipsoid
-##(empty field) time in seconds since last DGPS update
-#(empty field) DGPS station ID number
-#*47          the checksum data, always begins with *
+m <- leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  addCircleMarkers(lng=gps$lon, lat=gps$lat,radius = 1)
+m  # Print the map
+
+
+
